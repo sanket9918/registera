54a72ed1bfb85f7bffadcd3543fc36a9bba76445
sanket9918 <sanketmohapatra13@gmail.com>
2022-06-25 09:33:57 +0530

    Added docker

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
 
   const corsOptions = {
     // origin:"*", // for others
-    origin: ['http://localhost:3000',"http://10.0.2.2"], // for web and android emulator
+    origin: ['http://localhost:3000', 'http://10.0.2.2', 'http://127.0.0.1:3000'], // for web and android emulator
     credentials: true,
     exposedHeaders: ['Authorization'],
   };
